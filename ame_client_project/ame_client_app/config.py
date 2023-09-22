17cfc0cbc34dabdfa669ee2f82099ceeb1c58a99
jazzbox35 <jazzbox35@localhost.localdomain>
2023-09-21 20:41:44 -0700

    comments

--- ame_client_project/ame_client_app/config.py
+++ ame_client_project/ame_client_app/config.py
@@ -4,3 +4,7 @@ AME_API_KEY = '18574'
 # Enter your partition 
 AME_NODE = 'part2'
 
+# Devise custom logic which will be executed if the user clicks "GENERATE DEFAULT PROPOSITION"
+# This makes it possible for the front end UX to perform basic logic by creating the next proposition.
+#. However the user can still invoke the ame for more complicated (S2) scenarios.
+
