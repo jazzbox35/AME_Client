8bfc44a0ee2b7d3452ad01c3d97dfa769656933e
Mike Archbold <jazzbox35@gmail.com>
2023-09-22 18:03:46 -0700

    adding propositions so far

--- ame_client_project/ame_client_app/views.py
+++ ame_client_project/ame_client_app/views.py
@@ -8,22 +8,27 @@ from django.http import HttpResponse, JsonResponse
 import logging
 from ame_client_app.config import *
 
+propositions =[]
+
 logging.basicConfig(filename="ame_client_app.log",filemode='a',format='%(asctime)s:%(levelname)s:%(message)s',level=logging.INFO)
 
 def index(request):
     # Display home screen
+    global propositions
     data = {}
     next_action = 'display_home'
     return render(request, "ame_client_app/home.html", {"data": data, "next_action": next_action})
 
 def CreateNewCase(request):
-    # Mark case for training -- but no further updates allowed
+    global propositions
+    
     api_url = 'https://' + AME_NODE + '.agiengine.online/createcase'
     headers = {
           'api-key':  AME_API_KEY
       }
     data = {}
     try:
+        propositions =[]
         response = requests.put(api_url, headers=headers, json=data)
     except:
         return HttpResponse("Cannot communicate with AME server")
@@ -33,6 +38,7 @@ def CreateNewCase(request):
 
 def TrainCase(request):
     # Mark case for training -- but no further updates allowed
+    global propositions
     case = request.GET.get('case')
     api_url = 'https://' + AME_NODE + '.agiengine.online/traincase'
     headers = {
@@ -55,7 +61,8 @@ def TrainCase(request):
     return JsonResponse(data)
 
 def AddProposition(request):
-    
+    global propositions
+
     case = request.GET.get('case')
     proposition = request.GET.get('proposition')
     appearance = request.GET.get('appearance')
@@ -76,22 +83,25 @@ def AddProposition(request):
           "appearance"  :  appearance,
           "essence"     : "",
           "level"       : int(level),
-          "desired"     :  desired
+          "desired"     :  desired,
+          "system"      : system
         }
         logging.info(data)
     except:
         data ={}
 
     try:
+        propositions.insert(0,data)
         response = requests.put(api_url, headers=headers, json=data)
     except:
         return HttpResponse("Cannot communicate with AME server")
     data = response.json()
-    logging.info("proposition>" + str(data) + " status>" + str(response.status_code))
+    logging.info("proposition>" + str(data) + " status>" + str(response.status_code) + " props list>" + str(propositions))
     return JsonResponse(data)
 
 
 def Deliberate(request):
+    global propositions
     # Create level 1, system 2 judgment(s) and post to the server
     case = request.GET.get('case')
     api_url = 'https://' + AME_NODE + '.agiengine.online/sys2-realistic'
@@ -111,11 +121,15 @@ def Deliberate(request):
     except:
         return HttpResponse("Cannot communicate with AME server")
     data = response.json()
+    data['propositions'] = propositions
     logging.info("realistic call>" + str(data) + " status>" + str(response.status_code))
+    logging.info("RETURNING>" + str(data))
     return JsonResponse(data)
 
 def RetractProposition(request):
     # Retract the most recent proposition
+    global propositions
+
     case = request.GET.get('case')
     api_url = 'https://' + AME_NODE + '.agiengine.online/retract_that'
     headers = {
@@ -130,10 +144,12 @@ def RetractProposition(request):
         data ={}
     
     try:
+        # just clip the list of first
+        propositions = propositions[1:]
         response = requests.put(api_url, headers=headers, json=data)
     except:
         return HttpResponse("Cannot communicate with AME server")
     data = response.json()
-    logging.info("retract that>" + str(data) + " status>" + str(response.status_code))
+    logging.info("retract that>" + str(data) + " status>" + str(response.status_code) + " props list>" + str(propositions))
     return JsonResponse(data)
 
