cf4a0b385c678ac7460ee30ed9de3617c29a7a72
Mike Archbold <jazzbox35@gmail.com>
2023-10-03 14:22:19 -0700

    fixes

--- ame_client_project/ame_client_app/config.py
+++ ame_client_project/ame_client_app/config.py
@@ -1,8 +1,8 @@
 # Enter your customer key
-AME_API_KEY = '18574'
+AME_API_KEY = '00035'
 
 # Enter your partition 
-AME_NODE = 'part2'
+AME_NODE = 'apr6'
 
 # If you wish to insert custom logic to determine the next level proposition based on the preceding proposition(s),
 # TBD...
