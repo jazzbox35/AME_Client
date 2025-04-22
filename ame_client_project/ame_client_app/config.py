d736664682e7d4f234de01f6badfefa3936e1056
Mike Archbold <jazzbox35@gmail.com>
2025-04-22 11:03:10 -0700

    add loopback

--- ame_client_project/ame_client_app/config.py
+++ ame_client_project/ame_client_app/config.py
@@ -4,5 +4,9 @@ AME_API_KEY = '00035'
 # Enter your partition 
 AME_NODE = 'apr6'
 
+# if using loopback enter here and set switch to True (or False)
+LOOPBACK_TESTING_URL = 'http://127.0.0.1:8443'
+LOOPBACK_SWITCH = True
+
 # If you wish to insert custom logic to determine the next level proposition based on the preceding proposition(s),
 # TBD...
